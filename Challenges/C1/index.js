ddbca3eb5706d04fc0ec37a19e946d506689bcf1
harshitlamba <harshitlamba@yahoo.co.in>
2025-08-03 16:29:09 -0400

    When the user clicks the purchase button, render out 'Something went wrong, please try again' in the paragraph that has the id='error'.

---
+++ Challenges/C1/index.js
@@ -0,0 +1,8 @@
+let welcomeHl = document.getElementById("welcome-hl");
+
+let name = "Harshit";
+let greeting = "welcome to the Avengers!";
+
+welcomeHl.innerText = name + ", " + greeting;
+
+welcomeHl.innerText += " 🧛";
