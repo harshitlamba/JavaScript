a793c60b53cfa42c33d8e9dc6b8bc51d15492a8e
harshitlamba <harshitlamba@yahoo.co.in>
2025-08-03 16:33:17 -0400

    remove unwanted challenge - C1

--- Challenges/C1/index.js
+++
@@ -1,8 +0,0 @@
-let welcomeHl = document.getElementById("welcome-hl");
-
-let name = "Harshit";
-let greeting = "welcome to the Avengers!";
-
-welcomeHl.innerText = name + ", " + greeting;
-
-welcomeHl.innerText += " 🧛";
