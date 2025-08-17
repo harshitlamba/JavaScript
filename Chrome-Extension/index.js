d9065322d03ed1b66b2748b0a41d023106c82d0c
harshitlamba <harshitlamba@yahoo.co.in>
2025-08-17 19:55:47 -0400

    initial version job tracker chrome extension

--- Chrome-Extension/index.js
+++ Chrome-Extension/index.js
@@ -1,28 +1,68 @@
 const inputEl = document.getElementById("input-el");
-const inputBtn = document.getElementById("input-btn");
+const saveBtn = document.getElementById("save-btn");
+const saveTabBtn = document.getElementById("save-tab-btn");
+const deleteBtn = document.getElementById("delete-btn");
 const ulEl = document.getElementById("ul-el");
-
 let savedJobs = [];
+
+const jobsFromStorage = JSON.parse(localStorage.getItem("savedJobs"));
+
+if (jobsFromStorage) {
+    savedJobs = jobsFromStorage;
+    render(savedJobs);
+}
+
+function createLink(linkStr) {
+    const link = document.createElement("a");
+    link.textContent = linkStr;
+
+    if (!/^http:\/\//.test(linkStr)) {
+        linkStr = 'https://' + linkStr;
+    }
+    link.href = linkStr;
+    link.target = "_blank";
+
+    return link;
+}
+
+function render(jobs) {
+    ulEl.textContent = null;
+    if (jobs.length!=0) {
+        jobs.forEach(job => {
+        let joblink = createLink(job);
+        const newListEle = document.createElement("li");
+        newListEle.appendChild(joblink);
+        ulEl.append(newListEle);
+        });
+    }
+}
 
 inputEl.addEventListener("input", function() {
     this.style.backgroundColor = this.value.trim() !== "" ? "azure" : "";
 })
 
-inputBtn.addEventListener("click", function() {
-    if (inputEl.value != null && inputEl.value != "") {
-        savedJobs.push(inputEl.value);
-        inputEl.value = null;
+saveBtn.addEventListener("click", function() {
+    if (inputEl.value.trim() != "") {
+        let linkStr = inputEl.value.trim();
+        savedJobs.push(linkStr);
+        localStorage.setItem("savedJobs", JSON.stringify(savedJobs));
+        inputEl.value = "";
     }
-    renderJobs();
+    render(savedJobs);
 })
 
-function renderJobs() {
-    ulEl.textContent = null;
-    for (i = 0; i < savedJobs.length; i++) {
-        const newListEle = document.createElement("li")
-        newListEle.textContent = savedJobs[i];
-        ulEl.append(newListEle);
-    }
-}
+saveTabBtn.addEventListener("click", function() {
+    chrome.tabs.query({currentWindow: true, active: true}, function(tabs) {
+        savedJobs.push(tabs[0].url);
+        localStorage.setItem("savedJobs", JSON.stringify(savedJobs));
+        render(savedJobs);
+    });
+})
+
+deleteBtn.addEventListener("click", function() {
+    localStorage.clear();
+    savedJobs = [];
+    render(savedJobs);
+})
 
 
